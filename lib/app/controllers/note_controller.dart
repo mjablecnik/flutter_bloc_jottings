eb5411595a9eeece7cff399d990357e51efe7274
Martin Jablecnik <martin.jablecnik@email.cz>
2021-07-03 15:25:55 +0200

    feat: add flutter_quill editor

--- lib/app/controllers/note_controller.dart
+++ lib/app/controllers/note_controller.dart
@@ -1,38 +1,59 @@
+import 'dart:convert';
+
+import 'package:flutter/painting.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_quill/flutter_quill.dart';
 import 'package:jottings/app/common/constants.dart';
 import 'package:jottings/app/models/item.dart';
+import 'package:jottings/app/models/note.dart';
 
 enum Status { loading, success, failure }
 
 class NoteState {
   const NoteState._({
+    this.note,
+    this.errorMessage,
     this.status = Status.loading,
-    this.content = "",
   });
 
   const NoteState.loading() : this._();
 
-  const NoteState.success(String content)
-      : this._(status: Status.success, content: content);
+  const NoteState.success(Note note) : this._(note: note, status: Status.success);
 
-  const NoteState.failure() : this._(status: Status.failure);
+  const NoteState.failure(String message) : this._(status: Status.failure, errorMessage: message);
 
   final Status status;
-  final String content;
+  final Note? note;
+  final String? errorMessage;
 }
 
-
 class NoteController extends Cubit<NoteState> {
-
   final noteId;
+
+  late final editor;
 
   NoteController(this.noteId) : super(NoteState.loading()) {
     if (Item.getTypeFromId(noteId) == ItemType.Note) {
-      load(noteId);
+      load();
     }
   }
 
-  load(String folderId) {
-    emit(NoteState.success("Test text 123"));
+  save(note) {
+    note!.content = jsonEncode(editor.document.toDelta().toJson());
+    note.save();
+  }
+
+  load() {
+    var note = Note.load(noteId);
+    try {
+      var json = jsonDecode(note!.content);
+      editor = QuillController(
+        document: Document.fromJson(json),
+        selection: TextSelection.collapsed(offset: 0),
+      );
+    } catch (e) {
+      editor = QuillController.basic();
+    }
+    emit(NoteState.success(note!));
   }
 }
--- lib/app/pages/note_page.dart
+++ lib/app/pages/note_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_quill/widgets/editor.dart';
+import 'package:flutter_quill/widgets/toolbar.dart';
 import 'package:jottings/app/controllers/note_controller.dart';
 
 class NotePage extends StatelessWidget {
-
   final String noteId;
 
   final controller = Modular.get<NoteController>();
@@ -14,8 +15,36 @@ class NotePage extends StatelessWidget {
   @override
   Widget build(context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: Text("Test title")),
-      body: Center(child: Text(controller.state.content)),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(controller.state.note!.name),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.save),
+            onPressed: () {
+              controller.save(controller.state.note);
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Saved'),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          QuillToolbar.basic(controller: controller.editor),
+          Expanded(
+            child: Container(
+              child: QuillEditor.basic(
+                controller: controller.editor,
+                readOnly: false,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
